a5d7910b0b18fb97fd42370c68f1c6386335d711
VanessaLopezLU <leylopez32@gmail.com>
2024-02-26 17:27:50 -0500

    base de datos

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -1,17 +1,156 @@
 <!doctype html>
-<html lang="ar" dir="rtl">
+<html lang="es">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      body {
+          font-family: 'Arial', sans-serif;
+          background-color: #f4f4f4;
+          margin: 0;
+          padding: 0;
+      }
+
+      #contact-form {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #f8f8f8;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    #contact-form .image {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    #contact-form img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 5px;
+    }
+
+    #contact-form h2 {
+        color: #333;
+        text-align: center;
+    }
+
+    #contact-form form {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #contact-form label {
+        margin-bottom: 5px;
+        color: #555;
+    }
+
+    #contact-form input,
+    #contact-form button {
+        padding: 10px;
+        margin-bottom: 15px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+
+    #contact-form button {
+        background-color: #007bff;
+        color: #fff;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+    }
+
+    #contact-form button:hover {
+        background-color: #0056b3;
+    }
+      #menu-container {
+            
+            padding: 10px;
+            text-align: center;
+        }
+
+        .menu-item {
+            display: inline-block;
+            margin-right: 10px;
+            cursor: pointer;
+            color: #48cebc;
+            font-weight: bold;
+            font-size: 16px;
+            padding: 5px 10px;
+            border-radius: 5px;
+           
+            transition: background-color 0.3s ease;
+        }
+
+        .menu-item:hover {
+            background-color: #777;
+        }
+        .butto{
+          background-color: #48cebc;
+
+        }
+        #nosotros-content {
+        max-width: 800px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #f8f8f8;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    #nosotros-content img {
+        max-width: 100%;
+        height: auto;
+        border-radius: 5px;
+        margin-bottom: 15px;
+    }
+
+    #nosotros-content h2 {
+      color: #19b4c0;
+    }
+
+    #nosotros-content p {
+      color: #333;
+        
+        line-height: 1.6;
+        margin-bottom: 15px;
+    }
+
+    @media (max-width: 600px) {
+        #nosotros-content {
+            padding: 15px;
+        }
+    }
+  </style>
+    
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-dpuaG1suU0eT09tx5plTaGMLBsfDLzUCCUXOY2j/LSvXYuG6Bqs43ALlhIqAJVRb" crossorigin="anonymous">
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
 
-    <title>Crud Laravel 8</title>
+    <title>@yield('tituloPagina')</title>
   </head>
   <body>
-    <h1>Hola Mundo</h1>
+    <header>
+    
+      <center><h1>Tienda MT Motos</h1></center>
+  </header>
+
+  
+  <main>
+      
+      @yield('content')
+  </main>
+
+    
+
+
+     
+
+     
+    
 
     <!-- Optional JavaScript; choose one of the two! -->
 
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,100 @@
+@extends('layout.plantilla')
+
+@section('tituloPagina','Crud Laravel 8')
+
+@section('content')
+<div id="menu-container">
+    <div class="menu-item" id="inicio">Inicio</div>
+    <div class="menu-item" id="nosotros">Nosotros</div>
+    <div class="menu-item" id="contacto">Contacto</div>
+</div>
+
+<div id="inicio-content" class="con">
+    <center><img src="https://http2.mlstatic.com/D_NQ_NP_868157-MCO72507912494_102023-O.webp" alt="Imagen de nosotros"></center>
+</div>
+<center>
+<div id="nosotros-content" class="con">
+    <div class="image">
+        <h2>Nuestra Tienda MT Motos</h2>
+        <img src="https://http2.mlstatic.com/D_NQ_NP_868157-MCO72507912494_102023-O.webp" alt="Motos MT en nuestra tienda">
+    </div>
+    <p>
+        Bienvenido a nuestra tienda MT Motos, tu destino para encontrar las mejores motocicletas de la reconocida marca MT.
+        Nos enorgullece ofrecer una amplia selección de modelos, desde las potentes MT-07 hasta las emocionantes MT-09, 
+        diseñadas para satisfacer las necesidades de todos los amantes de las motos.
+    </p>
+    <p>
+        En MT Motos, no solo vendemos motocicletas; creamos experiencias. Nuestro equipo apasionado y conocedor está aquí 
+        para ayudarte a encontrar la moto perfecta que se adapte a tu estilo de conducción y preferencias. Ya seas un 
+        entusiasta experimentado o un principiante emocionado, tenemos algo para todos.
+    </p>
+    <p>
+        Además de nuestras excepcionales motos MT, ofrecemos una gama completa de accesorios, equipos de protección y 
+        servicios de mantenimiento para garantizar que disfrutes de cada viaje de manera segura y emocionante.
+    </p>
+    <p>
+        Visita nuestra tienda MT Motos hoy y descubre el apasionante mundo de la conducción en dos ruedas. ¡Estamos 
+        emocionados de ser parte de tu viaje en el mundo de las motocicletas MT!
+    </p>
+</div>
+</center>
+
+
+<div id="contact-form" class="con">
+    <div class="image">
+        <h2>Contacto</h2>
+        <img src="https://http2.mlstatic.com/D_NQ_NP_868157-MCO72507912494_102023-O.webp" alt="Imagen de inicio de sesión">
+    </div>
+
+    <div>
+        <form action="procesar_formulario.php" method="POST">
+            <label for="nombre">Nombre:</label>
+            <input type="text" id="nombre" name="nombre" required>
+
+            <label for="correo">Correo electrónico:</label>
+            <input type="email" id="correo" name="correo" required>
+
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required>
+
+            <label for="telefono">Teléfono:</label>
+            <input type="tel" id="telefono" name="telefono" required>
+ <br>
+            <center><button class="button" type="submit"><i class="fa fa-paper-plane"></i> Enviar</button></center>
+        </form>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.con').hide();
+
+        $('.menu-item').click(function () {
+            var id = $(this).attr('id');
+
+            // Ocultar todos los formularios y contenidos
+           
+            $('.con').hide();
+            switch (id) {
+                case 'inicio':
+                    $('#inicio-content').show();
+                    break;
+                case 'nosotros':
+                    $('#nosotros-content').show();
+                    break;
+                case 'contacto':
+                    $('#contact-form').show();
+                    break;
+                default:
+                    alert('Información no especificada');
+            }
+        });
+    });
+</script>
+
+
+
+@endsection
+
+
